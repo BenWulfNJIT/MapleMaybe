e4bb2149e1fbae1a7ddde79f65c8c73cea76c4ad
Benjamin Wulf <bw284@njit.edu>
2022-02-28 18:34:28 -0500

    can get platform count and row/column data from json map file array

--- src/vector_map.c
+++ src/vector_map.c
@@ -21,20 +21,42 @@ VectorMap *vectormap_new()
 VectorMap *vectormap_load(char *filename)
 {
     const char* backgroundFile = NULL;
-    SJson* json;
+    SJson *json, *rows, *columns;
     VectorMap* map;
+
+    int platformCount, columnCount, rowCount;
+
+
 
     if (!filename)return NULL;
     json = sj_load(filename);
     if (!json)return NULL;
+
     map = vectormap_new();
 
-    backgroundFile = sj_get_string_value(sj_object_get_value(json, "background"));
+
+
     map->testTest = sj_get_string_value(sj_object_get_value(json, "background"));
-    slog("Background Image: %s", map->testTest);
-    //char * backgroundFile = sj_object_get_value(json, "background");
-    //slog("Background file: ");
-    //slog(&backgroundFile);
+    
+    rows = sj_object_get_value(json, "plat1");
+    columns = sj_array_get_nth(rows, 0);
+
+    columnCount = sj_array_get_count(columns);
+    rowCount = sj_array_get_count(rows);
+
+    platformCount = rowCount / 2;
+
+
+    slog("Rows: %i", rowCount);
+    slog("Columns: %i", columnCount);
+    slog("Number of Platforms: %i", platformCount);
+
+    
+    //remember to free the json
+    //also need to add json platform data into an array, and also clear up this code a bit
+    // will probably be easiest to draw 1px wide rects instead of vectors? we'll see
+    //will need to possibly change format for map files
+    
 
 
 }
